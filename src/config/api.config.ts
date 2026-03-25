19960e5c052cbc3f2cc45c2c064c21e813b9a8c0
amochuko <7204868+amochuko@users.noreply.github.com>
2026-03-25 09:39:58 +0100

    add url in apiConfig

---
+++ src/config/api.config.ts
@@ -0,0 +1,12 @@
+export const apiConfig = {
+  url: {
+    auth: {
+      login: "auth/login",
+      signup: "auth/signup",
+      me: "auth/me",
+    },
+    sessions: "/sessions",
+    mentors: "/mentors",
+    payments: "/payments",
+  },
+};
